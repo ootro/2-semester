64cebdfa92d9bcd8c8afc677bf034d2cf14085c4
ootro <53119159+ootro@users.noreply.github.com>
2020-06-09 12:20:03 +0300

    Lab1
    added lab1

---
+++ lab1/C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace laba1
+{
+    class Program
+    {
+        public static void Increase(ref int x)
+        {
+            int bit = 1, counter = 0;
+            while (bit != 0)
+            {
+                bit = (x >> counter) & 1;
+                x ^= 1 << counter;
+                counter++;
+            }
+        }
+
+        public static int Compare(int a, int b)
+        {
+            int bitA, bitB;
+            for (int i = 0; i <= 31; i++)
+            {
+                bitA = (a >> i) & 1;
+                bitB = (b >> i) & 1;
+                if (bitA != bitB)
+                    return 0;
+            }
+            return 1;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Роботу виконав студент групи IС-93 факультету iнформатики та обчислювальної технiки Пiнчук Артур");
+            int a = 63, b = -18, c = 92;
+            Increase(ref a);
+            Console.WriteLine(a);
+            Console.WriteLine();
+            Console.WriteLine(Compare(b, c));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab1/C++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void increase(int* x) {
+	int bit = 1, counter = 0;
+	while (bit != 0) {
+		bit = (*x >> counter) & 1;
+		*x = *x ^ (1 << counter);
+		counter++;
+	}
+}
+
+bool compare(int a, int b) {
+	int bitA, bitB;
+		for (int i = 0; i <= 31; i++) {
+			bitA = (a >> i) & 1;
+			bitB = (b >> i) & 1;
+			cout << bitA << " " << bitB << endl;
+			if (bitA != bitB)
+				return 0;
+		}
+	return 1;
+}
+
+
+void main() {
+	setlocale(LC_ALL, "Rus");
+	cout << "Роботу виконав студент групи IС-93 факультету iнформатики та обчислювальної технiки Пiнчук Артур" << endl;
+	int a = 63, b = -18, c = 92;
+	increase(&a);
+	cout << a << endl << endl;
+	bool res = compare(b, c);
+	cout << res << endl;
+
+
+	system("pause");
+}
